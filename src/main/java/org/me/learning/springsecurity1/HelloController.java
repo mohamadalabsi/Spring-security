b3189a87a2cdae286fca8c5933d5657bd15322f6
mohamadalabsi <absimoda1@gmail.com>
2025-01-02 10:51:01 +0100

    printing the session ID and changing the password

--- src/main/java/org/me/learning/springsecurity1/HelloController.java
+++ src/main/java/org/me/learning/springsecurity1/HelloController.java
@@ -1,13 +1,16 @@
 package org.me.learning.springsecurity1;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+//    to get the session ID
     @GetMapping
-    public String hello() {
-        return "Hello World";
+    public String hello(HttpServletRequest request) {
+        return "Hello World" + request.getSession().getId();
     }
 }
