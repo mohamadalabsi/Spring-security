dae8ed9e5617a6b57d1162ba5f058549d6cfe58d
mohamadalabsi <absimoda1@gmail.com>
2025-01-23 23:52:31 +0100

    creating model and controller packages , and after finishing that Starting with CSRF Token

--- src/main/java/org/me/learning/springsecurity1/controller/HelloController.java
+++ src/main/java/org/me/learning/springsecurity1/controller/HelloController.java
@@ -1,7 +1,6 @@
-package org.me.learning.springsecurity1;
+package org.me.learning.springsecurity1.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +11,6 @@ public class HelloController {
     @GetMapping
     public String hello(HttpServletRequest request) {
         return "Hello World" + request.getSession().getId();
-        
+
     }
 }
---
+++ src/main/java/org/me/learning/springsecurity1/controller/StudentController.java
@@ -0,0 +1,4 @@
+package org.me.learning.springsecurity1.controller;
+
+public class StudentController {
+}
