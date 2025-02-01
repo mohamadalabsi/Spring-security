2f136d85f747f8be8b1475ce94dcd75755469a34
mohamadalabsi <absimoda1@gmail.com>
2025-02-01 23:59:34 +0100

    starting with CSRF Token

--- src/main/java/org/me/learning/springsecurity1/controller/StudentController.java
+++ src/main/java/org/me/learning/springsecurity1/controller/StudentController.java
@@ -1,5 +1,27 @@
 package org.me.learning.springsecurity1.controller;
 
+
+import org.me.learning.springsecurity1.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
-// here i will start with simple Apis
+
+//    we do not have a database rn so i will do this
+    private List <Student> students =new ArrayList<>(List.of(
+            new Student(1,"mohammad",90),
+            new Student(2,"malek",80)
+
+));
+
+  @GetMapping("/students")
+    public List <Student> getStudents(){
+      return students;
+    }
+
+
 }
