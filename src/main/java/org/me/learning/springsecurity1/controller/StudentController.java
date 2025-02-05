5302aa185a058383582375f3da396080aeccad64
mohamadalabsi <absimoda1@gmail.com>
2025-02-05 23:55:19 +0100

    finishing CSRF Token

--- src/main/java/org/me/learning/springsecurity1/controller/StudentController.java
+++ src/main/java/org/me/learning/springsecurity1/controller/StudentController.java
@@ -1,7 +1,9 @@
 package org.me.learning.springsecurity1.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.me.learning.springsecurity1.model.Student;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +21,19 @@ public class StudentController {
             new Student(2,"malek",80)
 
 ));
+//     because we use spring security we have to put the username and password for every Api
 
   @GetMapping("/student")
     public List <Student> getStudents(){
       return students;
     }
-//now here where CSRF Token take place
-//    we have to pass a Key and a Value
+//    now here where CSRF Token take place we do not use it for get , there is better alternative to this we will discuss it later 
+//    we have to pass a Key and a Value , we can get it from the browser or pass it in the code here
+@GetMapping("/csrf-token")
+public CsrfToken  getToken(HttpServletRequest request){
+    return (CsrfToken) request.getAttribute("_csrf");  //getAttribute is type Object , so we have to typecasting so it can be as CsrfToken , i will come back for this later
+}
+
     @PostMapping ("/student")
     public Student addStudent(@RequestBody Student student){
       students.add(student);
