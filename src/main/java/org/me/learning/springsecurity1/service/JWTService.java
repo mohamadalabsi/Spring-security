d785f624a867ca19c7c46e0ec4757a8f07a0d22d
mohamadalabsi <absimoda1@gmail.com>
2025-03-08 19:28:51 +0100

    finishing Generating JWT Token

--- src/main/java/org/me/learning/springsecurity1/service/JWTService.java
+++ src/main/java/org/me/learning/springsecurity1/service/JWTService.java
@@ -1,13 +1,52 @@
 package org.me.learning.springsecurity1.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.me.learning.springsecurity1.model.Users;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTService {
 
+    private  String secretKey ="" ;
 
-    public  String generateToken (){
-//        just example 
-        return "111111";
+    public JWTService() {
+        try {
+            KeyGenerator generator = KeyGenerator.getInstance("hmacSha256");
+            SecretKey sk = generator.generateKey(); // in Bytes
+            secretKey=Base64.getEncoder().encodeToString(sk.getEncoded()); // to string
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public  String generateToken (String username){
+//        just example
+//        return "111111";
+        Map<String , Object>  claims = new HashMap<>();
+        return Jwts.builder().claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() +60*60*10))  // it is in millisecond
+                .and()
+                .signWith(etKey())
+                .compact();
+    }
+
+    private Key etKey() {
+//        the key should be converted into bytes
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
